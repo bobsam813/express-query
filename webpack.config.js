5608312206e4937a96f304b4c350430ffa6650e2
Bob <gzyuboxian@corp.netease.com>
2016-10-21 17:52:46 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 const webpack = require('webpack');
 
 module.exports = {
+	target: 'electron',
 	entry: SRC_PATH + '/app.js',
 	output: {
 	    path: BUILD_PATH,
@@ -13,10 +14,10 @@ module.exports = {
 	},
 	module: {
 	    loaders: [
-	        { test: /\.css$/, loader: 'style!css', exclude: /node_modules/},
-	        { test: /\.html?$/, loader: 'html', exclude: /node_modules/ },
-	        { test: /\.vue$/, loader: 'vue', exclude: /node_modules/ },
-	        { test: /\.(png|jpg|gif)$/ig, loader: 'url?limit=8192&name=img/[name]-[hash:16].[ext]', exclude: /node_modules/}
+	        { test: /\.css$/, loader: 'style!css'},
+	        { test: /\.html?$/, loader: 'html' },
+	        { test: /\.vue$/, loader: 'vue' },
+	        { test: /\.(png|jpg|gif)$/ig, loader: 'url?limit=8192&name=img/[name]-[hash:16].[ext]'}
 	    ]
 	},
 	plugins: [
